7984cc2821c0d4a7d9b22b28a7ff7b399c1de0f7
Michael Davis <mhdavis1993@gmail.com>
2017-09-22 17:30:10 -0400

    server.js: added mongoose
    - server.js: restructured dependencies

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+// DEPENDENCIES
+// *******************************************************
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const exphbs = require("express-handlebars");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+mongoose.promise = Promise;
+// Scraping Tools
+const request = require("request");
+const cheerio = require("cheerio");
+// Required Models
+const Article = require("./app/models/Article.js");
+const Note = require("./app/models/Note.js");
 
 // Express app
 // =======================================================
@@ -9,7 +21,9 @@ let PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 // =======================================================
-app.use(bodyParser.urlencoded({ extended: true }));
+// Use morgan and body parser with app
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Static directory
@@ -18,7 +32,6 @@ app.use(express.static("public"));
 
 // Set Handlebars
 // =======================================================
-const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs(
   { defaultLayout: "main",
     layoutsDir: "./app/views/layouts"
@@ -27,8 +40,17 @@ app.engine("handlebars", exphbs(
 app.set('views', './app/views');
 app.set("view engine", "handlebars");
 
-
 // Import routes and give the server access to them.
 // =======================================================
 let routes;
 app.use("/", routes);
+
+// Mongoose Connection
+// =======================================================
+mongoose.connect("localhost");
+const db = mongoose.connection;
+
+// mongoose errors
+db.on("error", error => console.log(`Mongoose Error: ${error}`));
+// on success db login
+db.on("open", () => console.log("Mongoose connection successful."));
