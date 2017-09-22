6ff7521422d63dff820f3aee0b5f0cdfe708bbc3
Michael Davis <mhdavis1993@gmail.com>
2017-09-22 18:22:16 -0400

    Added contents to controllers + models
    - articles-controller.js: initiated file
    - notes-controller.js: initiated file
    - Article.js: populated with article schema
    - Note.js: populated with note schema

---
+++ app/models/Article.js
@@ -0,0 +1,29 @@
+// Require mongoose
+const mongoose = require("mongoose");
+// Create Schema class
+const Schema = mongoose.Schema;
+
+// Create article schema
+const ArticleSchema = new Schema({
+  // title is a required string
+  title: {
+    type: String,
+    required: true
+  },
+  // link is a required string
+  link: {
+    type: String,
+    required: true
+  },
+  // This only saves one note's ObjectId, ref refers to the Note model
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// Create the Article model with the ArticleSchema
+const Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
